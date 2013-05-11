5085e2040464ca85521047d512984e3ba7751e88
Megax <megax@yeahunter.hu>
2013-05-11 17:22:50 +0200

    * build.sh javítva.

--- build.sh
+++ build.sh
@@ -3,17 +3,25 @@
 build_conf="$(echo "$1" | tr [:upper:] [:lower:])"
 
 if [ "$2" = "" ]; then
-	build_platform="x86"
+	build_platform=""
 else
 	build_platform="$(echo "$2" | tr [:upper:] [:lower:])"
 fi
 
 if [ "$1" = "" ]; then
-	xbuild /p:Configuration="Debug" /p:Platform=$build_platform LuaInterface.sln
+	xbuild /p:Configuration="Debug" LuaInterface.sln
 else
-	if [ $build_conf = "release" ]; then
-		xbuild /p:Configuration="Release" /p:Platform=$build_platform LuaInterface.sln
+	if [ "$build_platform" = "" ]; then
+		if [ $build_conf = "release" ]; then
+			xbuild /p:Configuration="Release" LuaInterface.sln
+		else
+			xbuild /p:Configuration="Debug" LuaInterface.sln
+		fi
 	else
-		xbuild /p:Configuration="Debug" /p:Platform=$build_platform LuaInterface.sln
+		if [ $build_conf = "release" ]; then
+			xbuild /p:Configuration="Release" /p:Platform=$build_platform LuaInterface.sln
+		else
+			xbuild /p:Configuration="Debug" /p:Platform=$build_platform LuaInterface.sln
+		fi
 	fi
 fi
